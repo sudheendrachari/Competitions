5c23a0a398f5f09cbc9fb95172c05934fbb17ec6
sudheendrachari <sudheendrachari@users.noreply.github.com>
2016-06-04 21:25:19 +0530

    First draft

---
+++ graph/js/graph-controller.app.js
@@ -0,0 +1,48 @@
+(function () {
+	'use strict';
+	angular.module('app')
+		.controller('GraphController', GraphController);
+	function GraphController() {
+	    let scope = this;
+	    scope.name="asdf";
+	    scope.text = "asdfdsff";
+	    scope.generateGraph = generateGraph;
+	    scope.rerender = 0;
+	    scope.data = {nodes: [], links:[]};
+	    function letterDensity(name, text) {
+	    	let obj = {}, nameHash = {}, whiteSpace = /\s/g;
+    		name.replace(whiteSpace, '').split('').forEach(function(letter) {
+    			nameHash[letter] = true;
+    		});
+	    	text.replace(whiteSpace, '').split('').forEach(function(letter) {
+	    		if (nameHash[letter]) {
+		    		obj[letter] = obj[letter] || 0;
+	    			obj[letter]++;
+	    		}
+	    	});
+	    	return obj;
+	    }
+	    function generateGraph() {
+		    let density = letterDensity(scope.name, scope.text), g=1, letter, val = 0;
+		    	scope.data.nodes = [];
+		    	scope.data.links = [];
+		    for (letter in density) {
+		    	if (density.hasOwnProperty(letter)) {
+			    	scope.data.links.push({
+			    		source: g,
+			    		target: 0, 
+			    		value : 20 * density[letter]
+			    	});
+			    	scope.data.nodes.push({
+			    		name: letter,
+			    		group: ++g,
+			    		value: 5 * density[letter]
+			    	});
+		    		val += 5 * density[letter];
+		    	}
+		    }
+		    scope.data.nodes.unshift({"name": scope.name, group: g, value: 40});
+		    scope.rerender++;
+	    }
+	}
+})();
---
+++ graph/js/graph-directive.app.js
@@ -0,0 +1,96 @@
+(function () {
+	 'use strict';
+	angular.module('app')
+		.directive('myGraph', myGraph);
+		myGraph.$inject = [];
+		function myGraph() {
+		    const directive = {
+		            link: link,
+		            scope:{
+		            	data : '=',
+		            	render: '='
+		            },
+		            template: '<div id="graph"> </div>',
+		            restrict: 'E'
+		        };
+		    return directive;
+
+		    function link(scope, element, attrs) {
+	            let d3 = window.d3;
+            	let width = 960,
+            	    height = 500;
+
+            	let svg = d3.select("#graph").append("svg").attr("width", width).attr("height", height);
+	            function reDraw() {
+	            	svg.attr("width", window.innerWidth * 0.8).attr("height", 500);  
+            		renderGraph(scope.data);
+	            }
+	            window.addEventListener('resize', reDraw);
+	            scope.$watch('render', reDraw);	
+	            function renderGraph(graphData) {
+	            	svg.selectAll('*').remove();
+	            	let color = d3.scale.category20();
+
+	            	let force = d3.layout.force()
+	            	    .charge(-500)
+	            	    .linkDistance(function(link) {
+	            	    	return graphData.nodes[0].value +  link.value;
+	            	    })
+	            	    .size([width, height]);
+
+	            	    force
+	            	        .nodes(graphData.nodes)
+	            	        .links(graphData.links)
+	            	        .start()
+	            	        .friction(0.9);
+
+	            	    let link = svg.selectAll(".link")
+	            	        .data(graphData.links)
+	            	        .enter().append("line")
+	            	        .attr("class", "link")
+	            	        .style("stroke-width", 1);
+	            	    let gnodes = svg.selectAll('g.gnode')
+	            	       .data(graphData.nodes)
+	            	       .enter()
+	            	       .append('g')
+	            	       .classed('gnode', true);
+	            	    let node = gnodes.append("circle")
+	            	        .attr("class", "node")
+	            	        .attr("r", function(d){
+	            	        	return d.value;
+	            	        })
+	            	        .style("fill", function(d) {
+	            	            return color(d.group);
+	            	        })
+	            	        .call(force.drag);
+
+	            	    // gnodes.append("text")
+	            	    //     .text(function(d) {
+	            	    //         return d.name;
+	            	    //     });
+
+	            	    force.on("tick", function() {
+	            	        link.attr("x1", function(d) {
+	            	                return d.source.x;
+	            	            })
+	            	            .attr("y1", function(d) {
+	            	                return d.source.y;
+	            	            })
+	            	            .attr("x2", function(d) {
+	            	                return d.target.x;
+	            	            })
+	            	            .attr("y2", function(d) {
+	            	                return d.target.y;
+	            	            });
+
+	            	        node.attr("cx", function(d) {
+	            	                return d.x;
+	            	            })
+	            	            .attr("cy", function(d) {
+	            	                return d.y;
+	            	            });
+	            	    });
+	            }
+		    }
+		}
+})();
